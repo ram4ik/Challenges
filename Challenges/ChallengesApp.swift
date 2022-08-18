3f6dc1f4dd479c62e0cbed0b5d6cef16adc18755
Ramill <ramill_ibragimov@icloud.com>
2022-08-18 09:13:26 +0300

    Initial Commit

---
+++ Challenges/ChallengesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengesApp.swift
+//  Challenges
+//
+//  Created by Ramill Ibragimov on 18.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChallengesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
